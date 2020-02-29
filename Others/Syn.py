7c33a6d659205d7d64c3bcdd00cd475cc9b43f05
Karobben <591465908@qq.com>
2020-02-29 23:11:42 +0800

    Epi.png, Syn,py

--- Others/Syn.py
+++ Others/Syn.py
@@ -14,7 +14,21 @@ def Sheader(OUTPUT):
     F.write(Target)
     F.close()
 
+def header(OUTPUT):
+    Tample = open('O-index.html','r').read()
+    Sider_H = "<!-- Header Section Start -->"
+    Sider_T = "<!-- Header Section End -->"
+    Content = Tample[Tample.find(Sider_H):Tample.find(Sider_T)]
+    #
+    Target = open(OUTPUT,'r').read()
+    Target = Target[:Target.find(Sider_H)] +Content+ Target[Target.find(Sider_T):]
+    #
+    F = open(OUTPUT,'w')
+    F.write(Target)
+    F.close()
+
 
 List = os.popen('ls *.html').read().split('\n')[:-1]
 for i in List:
     Sheader(i)
+    header(i)
